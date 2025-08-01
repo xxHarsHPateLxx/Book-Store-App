b0b5060d5d4d8d732a57cd62aab890573c5c6476
Harsh Patel <harshpatel79871@gmail.com>
2025-08-01 17:27:10 +0530

    final commit

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { PORT } from "./config.js";
 import mongoose from "mongoose";
 import booksRoute from './routes/booksRoute.js'
 import cors from 'cors';
 
 
 const app = express()
+
+const PORT = process.env.PORT || 3000;
 
 // middleware for parsing request body
 app.use(express.json())
--- frontend/src/components/home/BooksCard.jsx
+++ frontend/src/components/home/BooksCard.jsx
@@ -1,10 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
-import { PiBookOpenTextLight } from 'react-icons/pi'
-import { BiUserCircle } from 'react-icons/bi'
-import { BsInfoCircle } from 'react-icons/bs'
-import { AiOutlineEdit } from 'react-icons/ai'
-import { MdOutlineDelete } from 'react-icons/md'
 import BookSingleCard from '../BookSingleCard';
 
 const BooksCard = ({books}) => {
